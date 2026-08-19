f6bad76465b75042551670fd7c9eb16ad52c929c
mehul <mehulanand78@gmail.com>
2026-08-19 20:14:55 +0530

    refactor: promote v3 to repo root, archive v1 to v1/

--- tools/weatherInfo.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const weatherInfo = async (cityname) => {
-  const url = `https://wttr.in/${cityname.toLowerCase()}?format=%C+%t`;
-  const { data } = await axios.get(url, { resposneType: "text" });
-  return `The current weather of ${cityname} is ${data}`;
-};
--- tools/webScraper.js
+++
@@ -1,9 +0,0 @@
-import * as cheerio from "cheerio";
-import axios from "axios";
-
-export const webScraper = async (siteLink="https://example.com") => {
-  const url = siteLink;
-  const response = await axios.get(url);
-  const data = cheerio.load(response.data);
-  console.log(data.html());
-};
